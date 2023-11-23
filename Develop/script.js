1595d7096c0f4b181f554ebad167524b2c0207f4
aalyman42 <aalyman42@gmail.com>
2023-11-22 22:00:16 -0700

    password processing now initiates after clicking the button rather than reload.

--- Develop/script.js
+++ Develop/script.js
@@ -85,50 +85,54 @@ var symbols = [
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var option0 = [];
 
-//CHECKING IF PASSWORD FITS REQUIRED SIZE//
-var acceptableLength = prompt("Please enter the amount of desired characters:");
-if (acceptableLength > 7 && acceptableLength < 129) {
-  //CHOOSING WHICH VARIABLE ARRAYS THE USER ALLOWS TO CONCATINATE INTO POSSIBLE VALUES//
-  //CHOOSING IF LOWERCASE VALUES ARE ALLOWED//
-  if (confirm("Would you like lowercase letters?")) {
-    var option1 = option0.concat(lowercaseText);
-    //USER DECLINES LOWERCASE VALUES//
+//Function Declaration for variable generatePassword//
+function generatePassword() {
+  //CHECKING IF PASSWORD FITS REQUIRED SIZE//
+  var acceptableLength = prompt(
+    "Please enter the amount of desired characters:"
+  );
+  if (acceptableLength > 7 && acceptableLength < 129) {
+    //CHOOSING WHICH VARIABLE ARRAYS THE USER ALLOWS TO CONCATINATE INTO POSSIBLE VALUES//
+    //CHOOSING IF LOWERCASE VALUES ARE ALLOWED//
+    if (confirm("Would you like lowercase letters?")) {
+      var option1 = option0.concat(lowercaseText);
+      //USER DECLINES LOWERCASE VALUES//
+    } else {
+      var option1 = option0;
+    }
+    //CHOOSING IF UPPERCASE VALUES ARE ALLOWED//
+    if (confirm("Would you like uppercase letters?")) {
+      var option2 = option1.concat(uppercaseText);
+      //USER DECLINES UPPERCASE VALUES//
+    } else {
+      var option2 = option1;
+    }
+    //CHOOSING IF SYMBOLS VALUES ARE ALLOWED//
+    if (confirm("Would you like symbols?")) {
+      var option3 = option2.concat(symbols);
+      //USER DECLINES SYMBOL VALUES//
+    } else {
+      var option3 = option2;
+    }
+    //CHOOSING IF NUMBER VALUES ARE ALLOWED//
+    if (confirm("Would you like numbers?")) {
+      var option4 = option3.concat(numbers);
+      //USER DECLINES NUMBER VALUES//
+    } else {
+      var option4 = option3;
+    }
+    console.log(option4);
+    //CHOOSING RANDOM VALUES FROM COMBINED LIST//
+    let n = 0;
+    while (n < acceptableLength) {
+      n++;
+      var yourPassword = option4[Math.floor(option4.length * Math.random())];
+      console.log(yourPassword);
+    }
   } else {
-    var option1 = option0;
+    alert("Please create a password between 8 and 128 characters.");
   }
-  //CHOOSING IF UPPERCASE VALUES ARE ALLOWED//
-  if (confirm("Would you like uppercase letters?")) {
-    var option2 = option1.concat(uppercaseText);
-    //USER DECLINES UPPERCASE VALUES//
-  } else {
-    var option2 = option1;
-  }
-  //CHOOSING IF SYMBOLS VALUES ARE ALLOWED//
-  if (confirm("Would you like symbols?")) {
-    var option3 = option2.concat(symbols);
-    //USER DECLINES SYMBOL VALUES//
-  } else {
-    var option3 = option2;
-  }
-  //CHOOSING IF NUMBER VALUES ARE ALLOWED//
-  if (confirm("Would you like numbers?")) {
-    var option4 = option3.concat(numbers);
-    //USER DECLINES NUMBER VALUES//
-  } else {
-    var option4 = option3;
-  }
-  console.log(option4);
-  //CHOOSING RANDOM VALUES FROM COMBINED LIST//
-  let n = 0;
-  while (n < acceptableLength) {
-    n++;
-    var yourPassword = option4[Math.floor(option4.length * Math.random())];
-    console.log(yourPassword);
-  }
-} else {
-  alert("Please create a password between 8 and 128 characters.");
 }
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
